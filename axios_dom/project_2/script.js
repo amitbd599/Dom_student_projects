1aa12b8363d5f6c4726a7c495714133b44b82516
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-09-12 19:59:20 +0600

    add axios project _2

---
+++ axios_dom/project_2/script.js
@@ -0,0 +1,27 @@
+// Fetch the data and populate the table when the page loads
+function data() {
+  const tableBody = document.querySelector('#tbody');
+
+  // Fetch data from API using axios
+  axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      const posts = response.data;
+
+      // Iterate over each post and add it to the table
+      posts.forEach(post => {
+        tableBody.innerHTML = tableBody.innerHTML + `
+            <tr>
+              <td>${post.id}</td>
+              <td>${post.title}</td>
+              <td>${post.body}</td>
+            </tr>
+          `;
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching posts:', error);
+    });
+};
+
+
+window.onload = data
